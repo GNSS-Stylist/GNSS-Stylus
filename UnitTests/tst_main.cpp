946d38e45e978d73531581f83fd4791fac55f87f
GNSS-Stylist <GNSSStylist@sci.fi>
2024-11-17 12:27:33 +0200

    Unit tests: Increase timeout for valgrind memory analyzer

--- UnitTests/tst_main.cpp
+++ UnitTests/tst_main.cpp
@@ -1,3 +1,21 @@
+/*
+    tst_main.cpp (part of GNSS-Stylus)
+    Copyright (C) 2024-present Pasi Nuutinmaki (gnssstylist<at>sci<dot>fi)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #include "tst_lidarfiltering.h"
 #include "tst_lazyevaluator.h"
 #include "tst_expressionfilter.h"
@@ -6,6 +24,9 @@
 int main(int argc, char **argv)
 {
     int status = 0;
+
+    // Some tests take a LONG time when using valgrind memory analyzer, so increase the timeout
+    qputenv("QTEST_FUNCTION_TIMEOUT", QByteArray("600000"));
 
     //-- run all tests
     {
