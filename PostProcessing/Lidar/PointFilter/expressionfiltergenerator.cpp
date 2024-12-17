1a53ffa3c0a242a15cb52ebcc88b15c030f8f714
GNSS-Stylist <GNSSStylist@sci.fi>
2024-12-17 12:30:40 +0200

    Fix ExpressionFilterGenerator.cpp's license

--- PostProcessing/Lidar/PointFilter/expressionfiltergenerator.cpp
+++ PostProcessing/Lidar/PointFilter/expressionfiltergenerator.cpp
@@ -1,5 +1,5 @@
 /*
-    expressionfiltergenerator.h (part of GNSS-Stylus)
+    expressionfiltergenerator.cpp (part of GNSS-Stylus)
     Copyright (C) 2024-present Pasi Nuutinmaki (gnssstylist<at>sci<dot>fi)
 
     This program is free software: you can redistribute it and/or modify
